327cf8f52e7d5fe9014ab715573935baa5ad7daf
mmunson94 <michaeljackmunson9994@outlook.com>
2023-07-09 20:40:54 +0000

    Edit text

--- run.py
+++ run.py
@@ -1,20 +1,22 @@
-import os
 from random import randint
+import os
 
 # Computer board for generated ship locations that is hidden from user
 computer_board = [[" "] * 9 for x in range(9)]
 # User Board that displays hits and misses
 user_board = [[" "] * 9 for i in range(9)]
 turns = 30
-name = Captain
+name = "Captain"
 
 # Function to randomly generate messages of support to the user
 def get_motivational_messages():
     messages = [
-        'keep trying Captain', 'almost got her! keep firing', 'all hands on deck', 'we need to sink these ships Captain!'
+        'keep trying Captain',
+        'almost got her! keep firing', 
+        'all hands on deck', 
+        'we need to sink these ships Captain!'
     ]
     return messages[randint(0, len(messages)-1)]
-
 
 # wipes the screen displayed to the user to give a better user experience
 def clear_screen():
@@ -48,6 +50,14 @@ letters_to_numbers = {
     'J': 8
 }
 
+# Function to generate ship locations on cpu board
+def plot_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,8), randint(0,8)
+        while board[ship_row][ship_column] == "X":
+            ship_row, ship_column = plot_coordinates()
+        board[ship_row][ship_column] = "X"
+
 # Function to take user input of coordinates for missile strikes
 def plot_coordinates():
     row = input("Enter the row of the ship: ").upper()
@@ -60,14 +70,6 @@ def plot_coordinates():
         column = input("Enter the column of the ship: ").upper()
     return int(row) - 1, letters_to_numbers[column]
 
-# Function to generate ship locations on cpu board
-def plot_ships(board):
-    for ship in range(5):
-        ship_row, ship_column = randint(0,8), randint(0,8)
-        while board[ship_row][ship_column] == "X":
-            ship_row, ship_column = plot_coordinates()
-        board[ship_row][ship_column] = "X"
-
 # Function to determine whether a ship has been hit
 def check_hits(board):
     count = 0
@@ -76,7 +78,6 @@ def check_hits(board):
             if column == "X":
                 count += 1
     return count
-
 
 # Funtion that starts the game
 def start_game():
@@ -88,13 +89,13 @@ def start_game():
         print('Guess a battleship location')
         row, column = plot_coordinates()
         if user_board[row][column] == "-":
-            print(", You guessed that one already.")
+            print(name + ", You guessed that one already.")
         elif computer_board[row][column] == "X":
             print("**** Hit ****")
             user_board[row][column] = "X" 
             turns -= 1  
         else:
-            print(", YOU MISSED!")
+            print(name + ", YOU MISSED!")
             user_board[row][column] = "-"   
             turns -= 1     
         if check_hits(user_board) == 5:
@@ -120,6 +121,7 @@ def restart_game():
     computer_board = [[" "] * 10 for x in range(9)]
     print(user_board)
     print(computer_board)
+    show_menu()
     start_game()
 
 
